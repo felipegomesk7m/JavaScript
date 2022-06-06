03b01d7c4cdd207b851b1b21b18e1cfd1ef2cd08
Felipe Gomes <felipegomesk7m@gmail.com>
2022-06-06 20:31:42 -0300

    feat.: adicionando novas funções do estado maior que 10 e para verificar se o número está negativo, mudando sua cor de fonte de texto para vermelho, adicionado detalhes do estilo da página

--- counter-app/counter-script-using-DOM.js
+++ counter-app/counter-script-using-DOM.js
@@ -1,12 +1,35 @@
 let currentNumberWrapper = document.getElementById('current-number');
+let limit = document.getElementById('limit');
 var currentNumber = 0;
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        estadoMaiorque10();
+        negativeNumber();
+    }
+} 
 
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    estadoMaiorque10();
+    negativeNumber();
+}
+
+function estadoMaiorque10() {
+    if (currentNumber >= 10) {
+        limit.style.display = "flex";
+    } else {
+        limit.style.display = "none";
+    }
+}
+
+function negativeNumber() {
+    if (currentNumber < 0) {
+        currentNumberWrapper.style.color = "#ff6347";
+    } else {
+        currentNumberWrapper.style.color = "black";
+    }
 }
