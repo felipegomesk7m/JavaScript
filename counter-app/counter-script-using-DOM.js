52a4b517af0a01f3e5a868a10a9dc89c7d046203
Felipe Gomes <felipegomesk7m@gmail.com>
2022-06-06 19:06:28 -0300

    feat.: funções dos botoões aumentar e diminuir

---
+++ counter-app/counter-script-using-DOM.js
@@ -0,0 +1,12 @@
+let currentNumberWrapper = document.getElementById('current-number');
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
